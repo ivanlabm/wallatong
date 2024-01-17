71e4c8aa34aa6af791f8b283f24c2f982b56215a
ivanlabm <148888808+ivanlabm@users.noreply.github.com>
2024-01-17 18:26:22 +0100

    otro cambio

--- modelos/config.php
+++ modelos/config.php
@@ -1,7 +1,11 @@
 <?php 
 
-$DB_USER= getenv('MYSQL_USER'));
-$DB_PASS= getenv('MYSQL_PASS'));
-$DB_DB= getenv('MYSQL_DB'));
-$DB_HOST getenv('MYSQL_HOST'));
+$DB_USER= getenv('MYSQL_USER');
+$DB_PASS= getenv('MYSQL_PASS');
+$DB_DB= getenv('MYSQL_DB');
+$DB_HOST getenv('MYSQL_HOST');
+
+$conn=mysqli_connect($DB_USER,$DB_PASS,$DB_DB,$DB_HOST);
+
+
 
