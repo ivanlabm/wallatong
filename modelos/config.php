04ec5bc630f877a7c978725d0080f406f2e9aa0f
ivanlabm <148888808+ivanlabm@users.noreply.github.com>
2024-01-17 18:17:10 +0100

    añadido getenv y eliminado connect no necesario

--- modelos/config.php
+++ modelos/config.php
@@ -1,19 +1,7 @@
 <?php 
-/*
-define('MYSQL_USER', 'root');
-define('MYSQL_PASS', '');
-define('MYSQL_DB', 'wallatong');
-define('MYSQL_HOST', 'localhost');
-*/
-define('MYSQL_USER', 'pyhhccgtjq');
-define('MYSQL_PASS', '00CXC4B8W7S1FE3Z$');
-define('MYSQL_DB', 'ejemploclaseivan-database');
-define('MYSQL_HOST', 'ejemploclaseivan-server.mysql.database.azure.com');
-/*
-$DB_HOST=gatenv("DB_HOST");
-$DB_USER=gatenv("DB_USERNAME");
-$DB_PASS=gatenv("DB_PASSWORD");
-$DB_DB=gatenv("DB_NAME");
-*/
 
-$connect=mysqli_connect($DB_HOST,$DB_USER,$DB_PASS,$DB_DB);
+define('MYSQL_USER', getenv('MYSQL_USER'));
+define('MYSQL_PASS', getenv('MYSQL_PASS'));
+define('MYSQL_DB', getenv('MYSQL_DB'));
+define('MYSQL_HOST', getenv('MYSQL_HOST'));
+
