60361f67074ce7b91c4836f2235220316839b67f
ivanlabm <148888808+ivanlabm@users.noreply.github.com>
2024-01-17 16:18:44 +0100

    Update config.php

--- modelos/config.php
+++ modelos/config.php
@@ -1,6 +1,13 @@
 <?php 
-
+/*
 define('MYSQL_USER', 'root');
 define('MYSQL_PASS', '');
 define('MYSQL_DB', 'wallatong');
 define('MYSQL_HOST', 'localhost');
+*/
+$DB_HOST=gatenv("DB_HOST");
+$DB_USER=gatenv("DB_USERNAME");
+$DB_PASS=gatenv("DB_PASSWORD");
+$DB_DB=gatenv("DB_NAME");
+
+$connect=mysqli_connect($DB_HOST,$DB_USER,$DB_PASS,$DB_DB);
